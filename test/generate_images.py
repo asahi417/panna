a7ca3805e3e2f3c219e8d3a29f08a98179304b78
asahi417 <asahi1992ushio@gmail.com>
2024-05-05 14:23:05 +0900

    init

--- test/generate_images.py
+++ test/generate_images.py
@@ -2,7 +2,7 @@ from time import time
 from panna import Diffuser, save_images
 
 prompts = [
-    "The philosopher, MF Doom, by Diego Velázquez",
+    "MF Doom by Diego Velázquez",
     "Study for a portrait of lady reading newspaper by Francis Bacon, the British painter"
 ]
 prompts_neg = [
@@ -14,14 +14,16 @@ model = Diffuser(enable_model_cpu_offload=False)
 if __name__ == '__main__':
     for i, (p, p_n) in enumerate(zip(prompts, prompts_neg)):
         start = time()
-        images, _ = model.text2image([p], batch_size=1)
+        refined_image_list, raw_image_list = model.text2image([p], batch_size=1)
         elapsed = time() - start
         print(f"{elapsed} sec")
-        save_images(images, "./test/test_images", file_prefix=f"generate_images.{i}")
+        save_images(raw_image_list, "./test/test_images", file_prefix=f"generate_images.{i}")
+        save_images(refined_image_list, "./test/test_images", file_prefix=f"generate_images.{i}.refined")
 
         start = time()
-        images, _ = model.text2image([p], negative_prompt=[p_n], batch_size=1)
+        refined_image_list, raw_image_list = model.text2image([p], negative_prompt=[p_n], batch_size=1)
         elapsed = time() - start
         print(f"{elapsed} sec")
-        save_images(images, "./test/test_images", file_prefix=f"generate_images.negative_prompt.{i}")
+        save_images(raw_image_list, "./test/test_images", file_prefix=f"generate_images.negative_prompt.{i}")
+        save_images(refined_image_list, "./test/test_images", file_prefix=f"generate_images.negative_prompt.{i}.refined")
 
