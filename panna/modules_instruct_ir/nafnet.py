d5d289fa58ba2f76c0e33d29eddd4e7292f2b3f3
asahi417 <asahi1992ushio@gmail.com>
2024-07-30 21:56:38 +0900

    add sd upscaler

--- panna/modules_instruct_ir/nafnet.py
+++ panna/modules_instruct_ir/nafnet.py
@@ -5,8 +5,7 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from .nafnet_utils import Local_Base, LayerNorm2d
+from .nafnet_utils import LayerNorm2d
 
 
 class SimpleGate(nn.Module):
@@ -33,39 +32,28 @@ class NAFBlock(nn.Module):
 
         # SimpleGate
         self.sg = SimpleGate()
-
         ffn_channel = FFN_Expand * c
         self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
         self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-
         self.norm1 = LayerNorm2d(c)
         self.norm2 = LayerNorm2d(c)
-
         self.dropout1 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
         self.dropout2 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
-
         self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
         self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
 
     def forward(self, inp):
         x = inp
-
         x = self.norm1(x)
-
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.sg(x)
         x = x * self.sca(x)
         x = self.conv3(x)
-
         x = self.dropout1(x)
-
         y = inp + x * self.beta
-
         x = self.conv4(self.norm2(y))
         x = self.sg(x)
         x = self.conv5(x)
-
         x = self.dropout2(x)
-
         return y + x * self.gamma
