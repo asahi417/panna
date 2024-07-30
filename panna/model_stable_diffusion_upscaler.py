ff22b6be8cf9049576388dd4dbd1a62acef67259
asahi417 <asahi1992ushio@gmail.com>
2024-07-30 20:36:23 +0900

    add sd upscaler

--- panna/model_stable_diffusion_upscaler.py
+++ panna/model_stable_diffusion_upscaler.py
@@ -3,16 +3,18 @@ from typing import Optional, Dict, List, Any
 import torch
 from diffusers import StableDiffusionUpscalePipeline
 from PIL.Image import Image
-from .util import get_generator, clear_cache, get_logger
+from .util import clear_cache, get_logger, resize_image
 
 logger = get_logger(__name__)
 
 
-class SD3:
+class SDUpScaler:
 
     config: Dict[str, Any]
     base_model_id: str
     base_model: StableDiffusionUpscalePipeline
+    height: int = 576
+    width: int = 1024
 
     def __init__(self,
                  base_model_id: str = "stabilityai/stable-diffusion-x4-upscaler",
@@ -30,31 +32,27 @@ class SD3:
         logger.info(f"pipeline config: {self.config}")
         self.base_model = StableDiffusionUpscalePipeline.from_pretrained(self.base_model_id, **self.config)
 
-    def text2image(self,
-                   prompt: List[str],
-                   batch_size: Optional[int] = None,
-                   negative_prompt: Optional[List[str]] = None,
-                   guidance_scale: float = 7.0,
-                   num_inference_steps: int = 28,
-                   num_images_per_prompt: int = 1,
-                   height: Optional[int] = None,
-                   width: Optional[int] = None,
-                   seed: Optional[int] = None) -> List[Image]:
-        """Generate image from text.
+    def image2image(self,
+                    images: List[Image],
+                    prompt: Optional[List[str]] = None,
+                    reshape_method: Optional[str] = None,
+                    upscale_factor: int = 4,
+                    batch_size: Optional[int] = None) -> List[Image]:
+        """Generate high resolution images from low resolution images.
 
+        :param images:
         :param prompt:
+        :param reshape_method:
+        :param upscale_factor:
         :param batch_size:
-        :param negative_prompt:
-        :param guidance_scale:
-        :param num_inference_steps: Define how many steps and what % of steps to be run on each expert (80/20) here.
-        :param num_images_per_prompt:
-        :param height:
-        :param width:
-        :param seed:
         :return:
         """
-        if negative_prompt is not None:
-            assert len(negative_prompt) == len(prompt), f"{len(negative_prompt)} != {len(prompt)}"
+
+        def downscale_image(image: Image) -> Image:
+            return resize_image(image, width=int(image.width / upscale_factor), height=int(image.height / upscale_factor))
+
+        prompt = [""] * len(images) if prompt is None else prompt
+        assert len(prompt) == len(images), f"{len(prompt)} != {len(images)}"
         batch_size = len(prompt) if batch_size is None else batch_size
         idx = 0
         output_list = []
@@ -62,16 +60,14 @@ class SD3:
             logger.info(f"[batch: {idx + 1}] generating...")
             start = idx * batch_size
             end = min((idx + 1) * batch_size, len(prompt))
-            output_list += self.base_model(
-                prompt=prompt[start:end],
-                negative_prompt=negative_prompt if negative_prompt is None else negative_prompt[start:end],
-                guidance_scale=guidance_scale,
-                num_images_per_prompt=num_images_per_prompt,
-                num_inference_steps=num_inference_steps,
-                height=height,
-                width=width,
-                generator=get_generator(seed)
-            ).images
+            batch = images[start:end]
+            if reshape_method == "best":
+                batch = [resize_image(i, width=self.width, height=self.height) for i in images]
+            elif reshape_method == "downscale":
+                batch = [downscale_image(i) for i in images]
+            elif reshape_method is not None:
+                raise ValueError(f"unknown reshape method: {reshape_method}")
+            output_list += self.base_model(image=batch, prompt=prompt[start:end]).images
             idx += 1
             clear_cache()
         return output_list
