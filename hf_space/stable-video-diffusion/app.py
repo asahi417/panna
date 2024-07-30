275a8678d77bdfd0b4f76e12813143826676014a
asahi417 <asahi1992ushio@gmail.com>
2024-07-30 19:50:45 +0900

    add svd

--- hf_space/stable-video-diffusion/app.py
+++ hf_space/stable-video-diffusion/app.py
@@ -19,11 +19,12 @@ title = ("# [Stable Video Diffusion](ttps://huggingface.co/stabilityai/stable-vi
 
 
 @spaces.GPU(duration=120)
-def infer(init_image, motion_bucket_id, noise_aug_strength, decode_chunk_size, fps, seed):
+def infer(init_image, num_frames, motion_bucket_id, noise_aug_strength, decode_chunk_size, fps, seed):
     base_count = len(glob(os.path.join(tmp_output_dir, "*.mp4")))
     video_path = os.path.join(tmp_output_dir, f"{base_count:06d}.mp4")
     frames = model.image2video(
         [init_image],
+        num_frames=num_frames,
         motion_bucket_id=motion_bucket_id,
         noise_aug_strength=noise_aug_strength,
         decode_chunk_size=decode_chunk_size,
@@ -43,19 +44,15 @@ with gr.Blocks() as demo:
         video = gr.Video()
     with gr.Accordion("Advanced options", open=False):
         seed = gr.Slider(label="Seed", minimum=0, maximum=1_000_000, step=1, value=0)
+        num_frames = gr.Slider(label="Number of frames", minimum=1, maximum=100, step=1, value=25)
         motion_bucket_id = gr.Slider(label="Motion bucket id", minimum=1, maximum=255, step=1, value=127)
-        noise_aug_strength = gr.Slider(label="Noise Strength", minimum=0, maximum=1, step=0.01, value=0.02)
+        noise_aug_strength = gr.Slider(label="Noise strength", minimum=0, maximum=1, step=0.01, value=0.02)
         fps = gr.Slider(label="Frames per second", minimum=5, maximum=30, step=1, value=6)
-        decode_chunk_size = gr.Slider(label="Frames per second", minimum=1, maximum=10, step=1, value=2)
+        decode_chunk_size = gr.Slider(label="Decode chunk size", minimum=1, maximum=10, step=1, value=2)
     run_button.click(
         fn=infer,
-        inputs=[image, motion_bucket_id, noise_aug_strength, decode_chunk_size, fps, seed],
+        inputs=[image, num_frames, motion_bucket_id, noise_aug_strength, decode_chunk_size, fps, seed],
         outputs=[video]
     )
-    gr.Examples(
-        fn=infer,
-        examples=examples,
-        inputs=image,
-        outputs=[video]
-    )
+    gr.Examples(examples=examples, inputs=image)
 demo.launch()
