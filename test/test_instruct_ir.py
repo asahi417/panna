54c0a3d4183c02078d7145ffe6e7706c0ae5cbc8
asahi417 <asahi1992ushio@gmail.com>
2024-07-30 22:05:37 +0900

    add sd upscaler

--- test/test_instruct_ir.py
+++ test/test_instruct_ir.py
@@ -10,5 +10,8 @@ os.makedirs("./test/test_image", exist_ok=True)
 output = model.image2image([sample_image])
 model.export(output[0], "./test/test_image/test_instruct_ir.png")
 
-output = model.image2image([sample_image], prompt=["a lady blinking"])
-model.export(output[0], "./test/test_image/test_instruct_ir.prompt.png")
+output = model.image2image([sample_image], prompt=["Correct the motion blur in this image so it is more clear"])
+model.export(output[0], "./test/test_image/test_instruct_ir.prompt1.png")
+
+output = model.image2image([sample_image], prompt=["please I want this image for my photo album, can you edit it as a photographer"])
+model.export(output[0], "./test/test_image/test_instruct_ir.prompt2.png")
