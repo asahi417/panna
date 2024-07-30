2e748c58f6484b2a038e78db69f00f0ffb3d0f00
asahi417 <asahi1992ushio@gmail.com>
2024-07-31 00:52:00 +0900

    add svd

--- test/test_pipeline_svd_upsacle.py
+++ test/test_pipeline_svd_upsacle.py
@@ -14,3 +14,12 @@ pipe(
     decode_chunk_size=8,
     seed=42,
 )
+
+pipe = PipelineSVDUpscale(upscaler="instruct_ir")
+pipe(
+    img,
+    prompt="Correct the motion blur in this image so it is more clear.",
+    output_path="./test/test_video/test_svd_upscale.instruct_ir.motion_bucket_id=127.noise_aug_strength=0.02.mp4",
+    decode_chunk_size=8,
+    seed=42,
+)
