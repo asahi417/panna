cbc98ec7e881db224921f4c4876de9e606529a41
asahi417 <asahi1992ushio@gmail.com>
2024-07-31 00:16:01 +0900

    add instructir

--- test/test_model_svd.py
+++ test/test_model_svd.py
@@ -1,16 +1,18 @@
 import os
 from PIL import Image
 from panna import SVD
+from diffusers.utils import load_image
 
 model = SVD()
 img = Image.open("./test/sample_image.png")
 os.makedirs("./test/test_video", exist_ok=True)
 
-data = model.image2video([img], seed=42)
-model.export(data[0], "./test/test_video/test_svd.motion_bucket_id=127.noise_aug_strength=0.02.mp4")
+data = model.image2video([img], seed=42, decode_chunk_size=16)
+model.export(data[0], "./test/test_video/test_svd.1.motion_bucket_id=127.noise_aug_strength=0.02.mp4")
 
-data = model.image2video([img], seed=42, motion_bucket_id=180)
-model.export(data[0], "./test/test_video/test_svd.motion_bucket_id=180.noise_aug_strength=0.02.mp4")
+data = model.image2video([img], seed=42, decode_chunk_size=16, motion_bucket_id=180)
+model.export(data[0], "./test/test_video/test_svd.1.motion_bucket_id=180.noise_aug_strength=0.02.mp4")
 
-data = model.image2video([img], seed=42, motion_bucket_id=180, noise_aug_strength=0.1)
-model.export(data[0], "./test/test_video/test_svd.motion_bucket_id=180.noise_aug_strength=0.1.mp4")
+img = load_image("https://huggingface.co/spaces/multimodalart/stable-video-diffusion/resolve/main/images/wink_meme.png")
+data = model.image2video([img], seed=42, decode_chunk_size=16)
+model.export(data[0], "./test/test_video/test_svd.2.motion_bucket_id=127.noise_aug_strength=0.02.mp4")
--- test/test_pipeline_svd_upsacle.py
+++ test/test_pipeline_svd_upsacle.py
@@ -5,25 +5,49 @@ from tqdm import tqdm
 from PIL.Image import Image
 from panna import SVD, SDUpScaler
 from panna.util import clear_cache
+from panna.pipeline import PipelineSVDUpscale
+
+img = load_image("https://huggingface.co/spaces/multimodalart/stable-video-diffusion/resolve/main/images/wink_meme.png")
+os.makedirs("./test/test_video", exist_ok=True)
+
+
+pipe = PipelineSVDUpscale()
+pipe(
+    img,
+    prompt="a lady in happy mood",
+    output_path="./test/test_video/test_svd_upscale.motion_bucket_id=127.noise_aug_strength=0.02.mp4",
+    decode_chunk_size=16,
+    seed=42,
+    motion_bucket_id=127,
+    noise_aug_strength=0.02
+)
+pipe(
+    img,
+    prompt="a lady in happy mood",
+    output_path="./test/test_video/test_svd_upscale.motion_bucket_id=160.noise_aug_strength=0.02.mp4",
+    decode_chunk_size=16,
+    seed=42,
+    motion_bucket_id=160,
+    noise_aug_strength=0.02
+)
 
 
 def pipeline_svd_upscale(
         image: Image,
         output_path: str,
         prompt: Optional[str] = None,
-        decode_chunk_size: int = 8,
         num_frames: int = 25,
-        motion_bucket_id: int = 180,
-        fps: int = 10,
-        noise_aug_strength: float = 0.1,
+        motion_bucket_id: int = 127,
+        fps: int = 7,
+        noise_aug_strength: float = 0.02,
         seed: Optional[int] = None):
-    svd = SVD(base_model_id="stabilityai/stable-video-diffusion-img2vid-xt-1-1")
+    svd = SVD()
     frames = svd.image2video(
         [image],
-        decode_chunk_size=decode_chunk_size,
         num_frames=num_frames,
         motion_bucket_id=motion_bucket_id,
         fps=fps,
+        decode_chunk_size=8,
         noise_aug_strength=noise_aug_strength,
         seed=seed
     )[0]
@@ -44,33 +68,20 @@ def pipeline_svd_upscale(
     clear_cache()
 
 
-img = load_image("https://huggingface.co/spaces/multimodalart/stable-video-diffusion/resolve/main/images/wink_meme.png")
-os.makedirs("./test/test_video", exist_ok=True)
-
 pipeline_svd_upscale(
     img,
     prompt="a lady in happy mood",
-    output_path="./test/test_video/test_svd_upscale.motion_bucket_id=120.noise_aug_strength=0.02.mp4",
+    output_path="./test/test_video/test_svd_upscale.motion_bucket_id=127.noise_aug_strength=0.02.mp4",
     seed=42,
-    motion_bucket_id=120,
+    motion_bucket_id=127,
     noise_aug_strength=0.02
 )
 
 pipeline_svd_upscale(
     img,
     prompt="a lady in happy mood",
-    output_path="./test/test_video/test_svd_upscale.motion_bucket_id=180.noise_aug_strength=0.02.mp4",
+    output_path="./test/test_video/test_svd_upscale.motion_bucket_id=160.noise_aug_strength=0.02.mp4",
     seed=42,
-    motion_bucket_id=180,
+    motion_bucket_id=160,
     noise_aug_strength=0.02
-)
-
-
-pipeline_svd_upscale(
-    img,
-    prompt="a lady in happy mood",
-    output_path="./test/test_video/test_svd_upscale.motion_bucket_id=180.noise_aug_strength=0.1.mp4",
-    seed=42,
-    motion_bucket_id=180,
-    noise_aug_strength=0.1
 )
