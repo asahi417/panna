f910151c0e89e1f315d9de7514d824232a7b7bf1
asahi417 <asahi1992ushio@gmail.com>
2024-08-01 01:05:44 +0900

    add ledits pp

--- test/test_pipeline_ledits_pp.py
+++ test/test_pipeline_ledits_pp.py
@@ -3,18 +3,18 @@ from panna.pipeline import PipelineLEditsPP
 
 
 img = Image.open("./test/sample_image.png")
-pipe = PipelineLEditsPP()
-pipe(img,
-     output_path="./test/test_image/test_ledits_pp.1.png",
-     edit_prompt=["lion", "cat"],
-     reverse_editing_direction=[True, False],
-     seed=42)
-pipe(img,
-     output_path="./test/test_image/test_ledits_pp.2.png",
-     edit_prompt=["lion", "cat"],
-     edit_style=["object", "object"],
-     reverse_editing_direction=[True, False],
-     seed=42)
+# pipe = PipelineLEditsPP()
+# pipe(img,
+#      output_path="./test/test_image/test_ledits_pp.1.png",
+#      edit_prompt=["lion", "cat"],
+#      reverse_editing_direction=[True, False],
+#      seed=42)
+# pipe(img,
+#      output_path="./test/test_image/test_ledits_pp.2.png",
+#      edit_prompt=["lion", "cat"],
+#      edit_style=["object", "object"],
+#      reverse_editing_direction=[True, False],
+#      seed=42)
 
 pipe = PipelineLEditsPP(use_refiner=True)
 pipe(img,
