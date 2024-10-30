493a6f492029553365a6162cfce2999f0b2621b0
asahi417 <asahi1992ushio@gmail.com>
2024-10-30 16:23:51 +0900

    init

---
+++ test/test_image_sdxl/test_model_sdxl_turbo_img2img.py
@@ -0,0 +1,19 @@
+import os
+from panna import SDXLTurboImg2Img
+
+
+def test(model, output_path, prefix):
+    os.makedirs(output_path, exist_ok=True)
+    sample_images = ["./test/sample_image_animal.png", "./test/sample_image_human.png"]
+    prompt = "geometric, modern, artificial, HQ, detail, fine-art"
+    negative_prompt = "low quality"
+    for i in sample_images:
+        output = model(image=[i], batch_size=1, seed=42)
+        model.export(output[0], f"{output_path}/{prefix}.{i}.png")
+        output = model(image=[i], prompt=[prompt], batch_size=1, seed=42)
+        model.export(output[0], f"{output_path}/{prefix}.{i}.prompt.png")
+        output = model(image=[i], prompt=[prompt], negative_prompt=[negative_prompt], batch_size=1, seed=42)
+        model.export(output[0], f"{output_path}/{prefix}.{i}.prompt.negative.png")
+
+
+test(SDXLTurboImg2Img(), "./test/test_image_sdxl/output", "test_sdxl_turbo_img2img")
