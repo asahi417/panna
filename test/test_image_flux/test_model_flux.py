604b71121e6ecd2e7f61db951d90c8befbf62e07
asahi417 <asahi1992ushio@gmail.com>
2024-11-01 17:20:39 +0900

    init

--- test/test_image_flux/test_model_flux.py
+++ test/test_image_flux/test_model_flux.py
@@ -8,7 +8,7 @@ def test(model, output_path, prefix):
         "A majestic lion jumping from a big stone at night",
         "A face of female model, high quality, fashion, Paris, Vogue, Maison Margiela, 8k"
     ]
-    for i, p in enumerate(zip(prompts)):
+    for i, p in enumerate(prompts):
         output = model([p], batch_size=1, seed=42)
         model.export(output[0], f"{output_path}/{prefix}.{i}.png")
         output = model([p], batch_size=1, seed=42, width=1024, height=720)
