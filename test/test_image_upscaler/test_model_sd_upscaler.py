e9cd8a00dbb9e4ad4595078234bc9c3d7503cc95
asahi417 <asahi1992ushio@gmail.com>
2024-11-04 00:53:11 +0900

    init

--- test/test_image_upscaler/test_model_sd_upscaler.py
+++ test/test_image_upscaler/test_model_sd_upscaler.py
@@ -8,10 +8,10 @@ def test(model, output_path, prefix):
     sample_image = "./test/sample_image_human_small.png"
     prompt = "Correct the blur in this image so it is more clear."
     init_image = load_image(sample_image)
-    output = model(image=[init_image], batch_size=1)
-    model.export(output[0], f"{output_path}/{prefix}.{os.path.basename(sample_image)}.png")
-    output = model(image=[init_image], prompt=[prompt], batch_size=1)
-    model.export(output[0], f"{output_path}/{prefix}.{os.path.basename(sample_image)}.prompt.png")
+    output = model(image=init_image)
+    model.export(output, f"{output_path}/{prefix}.{os.path.basename(sample_image)}.png")
+    output = model(image=init_image, prompt=prompt)
+    model.export(output, f"{output_path}/{prefix}.{os.path.basename(sample_image)}.prompt.png")
 
 
 test(SDUpScaler(), "./test/test_image_upscaler/output", "test_sd_upscaler")
--- test/test_text/test_model_clip_interrogator.py
+++ test/test_text/test_model_clip_interrogator.py
@@ -6,7 +6,7 @@ sample_images = ["./test/sample_image_animal.png", "./test/sample_image_human.pn
 model = CLIPInterrogator()
 output_file = "test/test_text/output.clip_interrogator.txt"
 for i in sample_images:
-    output = model([load_image(i)])
+    output = model(load_image(i))
     with open(output_file, "a") as f:
         f.write(f"CLIPInterrogator: {os.path.basename(i)}\n")
         for o in output:
