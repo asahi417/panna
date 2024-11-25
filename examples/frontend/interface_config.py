6b07e2bc4aaef5d24af373fa9a3632b38e72f37d
asahi417 <asahi1992ushio@gmail.com>
2024-11-25 20:10:50 +0900

    fix

--- examples/frontend/interface_config.py
+++ examples/frontend/interface_config.py
@@ -1,70 +1,110 @@
-import torch
-import gradio as gr
-import spaces
-from panna import Flux1Dev
-
 import os
 import requests
-from threading import Thread
-from pprint import pprint
+from typing import Dict, Any
+import gradio as gr
 
-# specify the endpoint you want to test
-endpoint = os.getenv("ENDPOINT", "http://0.0.0.0:4444")
+
+endpoint = os.getenv("ENDPOINT", None)
 if endpoint is None:
     raise ValueError("Endpoint not set.")
+endpoint = endpoint.split(",")
 
 
-# update config
+def validate_dict(d_1: Dict[str, Any], d_2: Dict[str, Any]) -> None:
+    for k in d_1.keys():
+        assert d_1[k] == d_2[k], f"{k} has different values: {d_1[k]}, {d_2[k]}"
+
+
 def update_config(
-        url,
         prompt,
         negative_prompt,
         noise_scale_latent_image,
         noise_scale_latent_prompt,
         alpha
-):
-    with requests.post(
-            f"{url}/update_config",
-            json={
-                "prompt": prompt,
-                "negative_prompt": negative_prompt,
-                "noise_scale_latent_image": noise_scale_latent_image,
-                "noise_scale_latent_prompt": noise_scale_latent_prompt,
-                "alpha": alpha
-            }
-    ) as r:
-        assert r.status_code == 200, r.status_code
-        pprint(r.json())
+) -> Dict[str, Any]:
+    config = None
+    for e in endpoint:
+        with requests.post(
+                f"{e}/update_config",
+                json={
+                    "prompt": prompt,
+                    "negative_prompt": negative_prompt,
+                    "noise_scale_latent_image": noise_scale_latent_image,
+                    "noise_scale_latent_prompt": noise_scale_latent_prompt,
+                    "alpha": alpha
+                }
+        ) as r:
+            assert r.status_code == 200, r.status_code
+            tmp_config = r.json()
+            if config is not None:
+                validate_dict(config, tmp_config)
+                config = tmp_config
+    return config
 
 
-model = Flux1Dev(torch_dtype=torch.bfloat16)
-title = ("# [Flux 1 Dev](https://huggingface.co/black-forest-labs/FLUX.1-dev)\n"
-         "The demo is part of [panna](https://github.com/asahi417/panna) project.")
-examples = [
-    "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k",
-    "A female model, high quality, fashion, Paris, Vogue, Maison Margiela, 8k",
-]
-css = """
-#col-container {
-    margin: 0 auto;
-    max-width: 580px;
-}
-"""
+def get_config() -> Dict[str, Any]:
+    config = None
+    for e in endpoint:
+        with requests.get(e) as r:
+            assert r.status_code == 200, r.status_code
+            tmp_config = r.json()
+            if config is not None:
+                validate_dict(config, tmp_config)
+                config = tmp_config
+    return config
 
-with gr.Blocks(css=css) as demo:
+
+default = get_config()
+# with gr.Blocks(css="#col-container {margin: 0 auto; max-width: 580px;}") as demo:
+with gr.Blocks() as demo:
+    # with gr.Column():
     with gr.Column(elem_id="col-container"):
-        gr.Markdown(title)
+        gr.Markdown("# Tuning Interface for Img2Img Generation")
+        prompt = gr.Text(
+            label="Prompt",
+            max_lines=2,
+            placeholder="Enter your prompt.",
+            value=default["prompt"]
+        )
+        negative_prompt = gr.Text(
+            label="Negative Prompt",
+            max_lines=2,
+            placeholder="Enter your negative prompt.",
+            value=default["negative_prompt"]
+        )
+        noise_scale_latent_image = gr.Slider(
+            label="Noise Scale (Image)",
+            minimum=0.0,
+            maximum=1.0,
+            step=0.01,
+            value=float(default["noise_scale_latent_image"])
+        )
+        noise_scale_latent_prompt = gr.Slider(
+            label="Noise Scale (Prompt)",
+            minimum=0.0,
+            maximum=1.0,
+            step=0.01,
+            value=float(default["noise_scale_latent_prompt"])
+        )
+        alpha = gr.Slider(
+            label="Alpha",
+            minimum=0.0,
+            maximum=1.0,
+            step=0.01,
+            value=float(default["alpha"])
+        )
+        seed = gr.Slider(
+            label="Seed",
+            minimum=0,
+            maximum=1_000_000,
+            step=1,
+            value=int(default["seed"])
+        )
         with gr.Row():
-            prompt = gr.Text(label="Prompt", show_label=False, max_lines=1, placeholder="Enter your prompt", container=False)
-            run_button = gr.Button("Run", scale=0)
-        result = gr.Image(label="Result", show_label=False)
-        seed = gr.Slider(label="Seed", minimum=0, maximum=1_000_000, step=1, value=0)
-        with gr.Row():
-            width = gr.Slider(label="Width", minimum=256, maximum=1344, step=64, value=1024)
-            height = gr.Slider(label="Height", minimum=256, maximum=1344, step=64, value=1024)
-        with gr.Row():
-            guidance_scale = gr.Slider(label="Guidance scale", minimum=0.0, maximum=10.0, step=0.1, value=3.5)
+
             num_inference_steps = gr.Slider(label="Inference steps", minimum=1, maximum=50, step=1, value=28)
+        run_button = gr.Button("Run", scale=0)
+        result = gr.Image(label="Result", show_label=False)
         gr.Examples(examples=examples, inputs=[prompt])
     gr.on(
         triggers=[run_button.click, prompt.submit],
