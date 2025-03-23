d49d2323e5e2ae97980c2e3e8f48271b77c2e4cd
asahi417 <asahi1992ushio@gmail.com>
2025-03-23 16:33:58 +0000

    add nervous

--- panna/__init__.py
+++ panna/__init__.py
@@ -1,9 +1,9 @@
 # image generation models
-from .model_txt2img_image_stable_diffusion_xl import (
+from .model_txt2img_stable_diffusion_xl import (
     SDXLTurbo,
     SDXLTurboImg2Img,
 )
-from .model_txt2img_image_stable_diffusion_3 import (
+from .model_txt2img_stable_diffusion_3 import (
     SD3LargeTurbo,
     SD3LargeTurbo4Bit,
 )
